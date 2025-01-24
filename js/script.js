9378417ef2e160eae9e74d0b02d8ce0b1dd4314b
Tamillk <122620722+Tamillk@users.noreply.github.com>
2025-01-24 20:49:48 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+function showAlert() {
+    alert("We offer advanced cybersecurity solutions to protect your business!");
+}
+
+function contactUs() {
+    alert("Contact us at: info@cybersecure.com");
+}
