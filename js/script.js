d5d79f6e8aa11f66d8cd878eb78bf8b564ff5ec6
Tamillk <122620722+Tamillk@users.noreply.github.com>
2025-01-24 21:14:28 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-function showAlert() {
-    alert("We offer advanced cybersecurity solutions to protect your business!");
-}
-
-function contactUs() {
-    alert("Contact us at: contact@lexcyberwall.com");
-}
